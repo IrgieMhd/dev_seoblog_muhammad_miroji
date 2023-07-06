d69efa57c613bc855750a70374e77eff4266d136
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-06 10:26:08 +0700

    signup route and controllers setup

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 // bring routes
 const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
 
 // app
 const app = express();
@@ -27,7 +28,8 @@ if(process.env.NODE_ENV == 'development') {
 }
 
 // routes middleware
-app.use(blogRoutes);
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
 // port
 const port = process.env.PORT || 8000
