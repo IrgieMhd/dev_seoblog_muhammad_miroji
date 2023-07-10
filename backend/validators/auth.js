fda17b8831625248a593ecee2d7002784cf1a9f9
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-10 14:18:13 +0700

    Giving Validators

---
+++ backend/validators/auth.js
@@ -0,0 +1,7 @@
+const { body } = require('express-validator');
+
+exports.userSignupValidator = [
+  body('name').not().isEmpty().withMessage('Name is required'),
+  body('email').isEmail().withMessage('Must be valid email address'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 charachter long')
+]
