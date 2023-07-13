0f695cd89af977aea1c3b4661b36bf177c10c489
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-13 10:29:12 +0700

    Create signin from backend

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -5,3 +5,8 @@ exports.userSignupValidator = [
   body('email').isEmail().withMessage('Must be valid email address'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 charachter long')
 ]
+
+exports.userSigninValidator = [  
+  body('email').isEmail().withMessage('Must be valid email address'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 charachter long')
+]
