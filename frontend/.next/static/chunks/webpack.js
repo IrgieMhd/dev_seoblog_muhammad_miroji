3834abbf26965cfc8e53c3fe36d0db06bb81b62b
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-20 12:12:22 +0700

    Category read delete & messages

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fe89b8368b99fc6e"; }
+/******/ 		__webpack_require__.h = function() { return "a1282bcd3b1817d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
