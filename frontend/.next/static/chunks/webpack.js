d22e573d52265010fe74054047b743bba3efc22e
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-26 18:33:24 +0700

    Single blog with fullwidth image & content

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57db0c4172be4332"; }
+/******/ 		__webpack_require__.h = function() { return "f948d57c058af38b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
