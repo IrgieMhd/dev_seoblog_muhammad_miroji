2d5f88c03b0561f484b3b51722f1b269404552e3
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-26 19:06:46 +0700

    Single blog head section for SEO

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f948d57c058af38b"; }
+/******/ 		__webpack_require__.h = function() { return "614dd8964f8e5a9a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
