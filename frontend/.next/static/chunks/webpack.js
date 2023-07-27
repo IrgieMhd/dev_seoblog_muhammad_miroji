2a8e4330cb8e7bb9a53b4658502a039aab821e73
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-27 11:09:06 +0700

    List of blogs for Admin (Frontend)

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "92f94241e7f4e7c7"; }
+/******/ 		__webpack_require__.h = function() { return "5d7ae3bbc6d6233c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
