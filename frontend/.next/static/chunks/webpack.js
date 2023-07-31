0179f2c5e7151b0d5dc1ffc711a892e5e0cb5e75
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-07-31 14:28:41 +0700

    Profile update actions & page setup

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f20a95422a30831"; }
+/******/ 		__webpack_require__.h = function() { return "aac7d15793567688"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
