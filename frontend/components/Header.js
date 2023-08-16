019c114e9fdf0083596dd280b8b5396d7666e07f
irgie.dandinote@gmail.com <irgie.dandinote@gmail.com>
2023-08-16 11:04:08 +0700

    Using google analytics

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -93,9 +93,9 @@ const Header = () => {
             )}
 
             <NavItem>
-              <Link legacyBehavior href="/user/crud/blog">
-                <NavLink className="btn btn-primary text-light">Write a blog</NavLink>
-              </Link>
+              <a href="/user/crud/blog" className="btn btn-primary text-light">
+                Write a blog
+              </a>
             </NavItem>
 
           </Nav>
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
 export default function Document() {
+
+  const setGoogleTags = () => {
+    if (publicRuntimeConfig.PRODUCTION) {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7JST3GMGZV');
+        `
+      };
+    }
+  }
+
   return (
     <Html lang="en">
       <Head>
@@ -11,6 +29,10 @@ export default function Document() {
           href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.2/css/bootstrap.min.css" />
         {/*<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />*/}
         <link rel="stylesheet" href="/static/global.css" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7JST3GMGZV"></script>
+        <script dangerouslySetInnerHTML={setGoogleTags()} />
+
       </Head>
       <body>
         <Main />
